56c595931759021d20e75cb24727811b6860462b
Elijah Roussos <elijah.rou@gmail.com>
2022-07-22 18:10:32 +0200

    upgrade to v1 bentoml

--- fraud_detection_service.py
+++ fraud_detection_service.py
@@ -1,21 +1,18 @@
+import numpy as np
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
-from xgboost import XGBClassifier
 
-from bentoml import env, artifacts, api, BentoService
-from bentoml.adapters import DataframeInput, JsonOutput
-from bentoml.frameworks.sklearn import SklearnModelArtifact
+import bentoml
+from bentoml.io import PandasDataFrame, JSON
 
-@env(infer_pip_packages=True)
-@artifacts([SklearnModelArtifact('model'), SklearnModelArtifact('encoder')])
-class FraudClassifier(BentoService):
-    @api(input=DataframeInput(), output=JsonOutput(), batch=True)
-    def predict(self, df):
-        model = self.artifacts.model
-        enc = self.artifacts.encoder
+ohe_encoder = bentoml.models.get("fraud_classifier:latest").custom_objects["ohe_encoder"]
+fraud_classifier_runner = bentoml.sklearn.get("fraud_classifier:latest").to_runner()
 
-        X = df[["ProductCD", "P_emaildomain", "R_emaildomain", "card4", "M1", "M2", "M3"]]
-        X = X.fillna(pd.NA) # ensure all missing values are pandas NA
-        X = pd.DataFrame(enc.transform(X).toarray(), columns=enc.get_feature_names_out().reshape(-1))
-        X["TransactionAmt"] = df[["TransactionAmt"]].to_numpy()
-        return model.predict(X)
+svc = bentoml.Service("fraud_classifier", runners=[fraud_classifier_runner])
+
+@svc.api(input=PandasDataFrame(), output=JSON(), route="/fraud-classifier")
+def predict(df: pd.DataFrame) -> np.ndarray:
+    X = df[["ProductCD", "P_emaildomain", "R_emaildomain", "card4", "M1", "M2", "M3"]]
+    X = X.fillna(pd.NA) # ensure all missing values are pandas NA
+    X = pd.DataFrame(ohe_encoder.transform(X).toarray(), columns=ohe_encoder.get_feature_names_out().reshape(-1))
+    X["TransactionAmt"] = df[["TransactionAmt"]].to_numpy()
+    return fraud_classifier_runner.predict.run(X)
