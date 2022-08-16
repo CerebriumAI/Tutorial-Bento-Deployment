47681ddd5cc4fccbe449303061ac34c6ccf9cbf4
Elijah Roussos <elijah.rou@gmail.com>
2022-08-16 17:16:38 +0100

    neaten file

--- train.py
+++ train.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 from xgboost import XGBClassifier
+from sklearn.model_selection import train_test_split
+import bentoml
 
 # Load the data, sample such that the target classes are equal size
 df = pd.read_csv("data/train_transaction.csv")
@@ -24,11 +26,10 @@ X = pd.DataFrame(
 X["TransactionAmt"] = df[["TransactionAmt"]].to_numpy()
 
 # Split the dataset and train the model
-from sklearn.model_selection import train_test_split
-
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+
 xgb = XGBClassifier(
     n_estimators=100,
     learning_rate=0.1,
@@ -44,9 +45,7 @@ xgb = XGBClassifier(
 )
 model = xgb.fit(X_train, y_train)
 
-
-import bentoml
-
+# Save the models
 saved_model = bentoml.sklearn.save_model(
     "fraud_classifier",
     model,
